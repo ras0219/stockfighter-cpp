ccaadd5a2880b8323d5f594ecd02569ae62ab9da
Robert Schumacher <robertallenschumacher@gmail.com>
2015-12-13 21:21:29 -0800

    Basic request working

--- stockfighter-cpp/main.cpp
+++ stockfighter-cpp/main.cpp
@@ -4,7 +4,7 @@
 #include <cpprest/http_client.h>
 
 using namespace web::http;
-
+using namespace client;
 
 using namespace std;
 
@@ -13,14 +13,22 @@ void fatal(const char* msg) {
   abort();
 }
 
-int main() {
-  ifstream f("~/.stockfighter");
+int main(int argc, const char** argv) {
+  if (argc < 2) fatal("usage: main <apikeyfile>");
+  ifstream f(argv[1]);
   if (!f) fatal("error: ifstream()");
   string apikey;
   if (!getline(f, apikey)) fatal("error: getline()");
 
-  
+  cout << "API Key: '" << apikey << "'\n";
 
-  printf("Hello, world!\n");
+  http_client client("https://api.stockfighter.io");
+
+  auto res = client.request("GET", "/ob/api/venues").get();
+
+  auto body = res.extract_utf8string(true).get();
+
+  cout << body << "\n===END OF MESSAGE===\n";
+
   return 0;
 }
