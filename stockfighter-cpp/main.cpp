eaff0b45807943ef5ed602c91e5bad543d522d1a
Robert Schumacher <robertallenschumacher@gmail.com>
2015-12-13 21:04:14 -0800

    Fix build on linux

--- stockfighter-cpp/main.cpp
+++ stockfighter-cpp/main.cpp
@@ -1,9 +1,26 @@
-#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
 #include <cpprest/http_client.h>
 
 using namespace web::http;
 
+
+using namespace std;
+
+void fatal(const char* msg) {
+  cerr << msg << "\n";
+  abort();
+}
+
 int main() {
-	printf("Hello, world!\n");
-	return 0;
+  ifstream f("~/.stockfighter");
+  if (!f) fatal("error: ifstream()");
+  string apikey;
+  if (!getline(f, apikey)) fatal("error: getline()");
+
+  
+
+  printf("Hello, world!\n");
+  return 0;
 }
