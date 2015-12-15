0bba841bdaf806c616c9a33a1e8983bc07a6e04c
Robert Schumacher <robertallenschumacher@gmail.com>
2015-12-14 21:48:20 -0800

    Refactoring more into rval parser

--- stockfighter-cpp/sf_client.cpp
+++ stockfighter-cpp/sf_client.cpp
@@ -20,114 +20,6 @@ sf_client::sf_client(string apikey, string host)
 
 sf_client::~sf_client() {}
 
-optional<unit> sf_client::api_heartbeat() {
-  TRACE;
-  optional<unit> ret;
-
-  auto res = m_client->request("GET", "/ob/api/heartbeat").get();
-  auto doc = res.extract_json().get();
-  auto& obj = doc.as_object();
-  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
-  switch (ret.ok) {
-  case OK: break;
-  case ERROR:
-    ret.err = obj["error"].as_string();
-    break;
-  }
-
-  TRACE;
-  return ret;
-}
-
-optional<symbolslist_t> sf_client::venue_stocks(const std::string& venue) {
-  TRACE;
-  optional<symbolslist_t> ret;
-
-  auto res = m_client->request("GET", "/ob/api/venues/"+venue+"/stocks").get();
-  auto doc = res.extract_json().get();
-  auto& obj = doc.as_object();
-  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
-  switch (ret.ok) {
-  case ERROR:
-    ret.err = obj["error"].as_string();
-    break;
-  case OK: {
-    auto& arr = obj["symbols"].as_array();
-    for (auto&& sym : arr) {
-      auto& symobj = sym.as_object();
-      ret.data.symbols.push_back(symboldef_t{
-	  symobj["symbol"].as_string(),
-	    symobj["name"].as_string()
-	    });
-    }
-    break;
-  }
-  }
-
-  TRACE;
-  return ret;
-}
-
-void parse_into(const json::array& arr, std::vector<request_t>& reqs) {
-  for (auto&& req : arr) {
-    auto& reqobj = req.as_object();
-    reqs.push_back(request_t{
-	reqobj.at("price").as_number().to_uint64(),
-	  reqobj.at("qty").as_number().to_uint64()});
-  }
-}
-
-optional<orderbook_t> sf_client::orderbook_for_venue(const std::string& venue, const std::string& stock) {
-  TRACE;
-  optional<orderbook_t> ret;
-  
-  auto res = m_client->request("GET", "/ob/api/venues/"+venue+"/stocks/"+stock).get();
-  auto doc = res.extract_json().get();
-
-  auto& obj = doc.as_object();
-  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
-  switch (ret.ok) {
-  case ERROR:
-    ret.err = obj["error"].as_string();
-    break;
-  case OK: {
-    if (!obj["bids"].is_null()) parse_into(obj["bids"].as_array(), ret.data.bids);
-    if (!obj["asks"].is_null()) parse_into(obj["asks"].as_array(), ret.data.asks);
-    ret.data.timestamp = obj["ts"].as_string();
-    break;
-  }
-  }
-
-  TRACE;
-  return ret;
-}
-
-json::value order_to_json(const order_request_t& order) {
-  std::vector<std::pair<std::string, json::value>> fields;
-  fields.emplace_back("account", json::value::string(order.account));
-  fields.emplace_back("venue", json::value::string(order.venue));
-  fields.emplace_back("stock", json::value::string(order.symbol));
-  fields.emplace_back("price", json::value::number(order.req.price));
-  fields.emplace_back("qty", json::value::number(order.req.qty));
-  fields.emplace_back("direction", json::value::string(order.direction == BUY ? "buy" : "sell"));
-  fields.emplace_back("orderType", json::value::string(stringify(order.type)));
-  return json::value::object(move(fields));
-}
-
-template<class F>
-vector<result_of_t<F(const json::value&)>> map(const json::value& v, F f) {
-  using v_t = vector<result_of_t<F(const json::value&)>>;
-  v_t ret;
-
-  if (v.is_null())
-    return ret;
-
-  for (auto&& e : v.as_array())
-    ret.push_back(f(e));
-  
-  return ret;
-}
-
 // This is a parser based on return type deduction.
 // I am not sure how I feel about this... but it does make for very elegant code.
 struct parser {
@@ -143,11 +35,23 @@ struct parser {
   explicit operator string() const {
     return val.as_string().c_str();
   }
+
+  operator unit() const { return unit(); }
   
   operator uint64_t() const {
     return val.as_number().to_uint64();
   }
 
+  operator vector<string>() const {
+    vector<string> ret;
+    if (val.is_null())
+      return ret;
+    for (auto&& e : val.as_array())
+      ret.push_back(string(parser{e}));
+
+    return ret;
+  }
+  
   template<class T>
   operator vector<T>() const {
     vector<T> ret;
@@ -213,7 +117,112 @@ struct parser {
     ret.data = *this;
     return ret;
   }
+
+  operator symboldef_t() const {
+    symboldef_t ret;
+    ret.symbol = string(at("symbol"));
+    ret.name = string(at("name"));
+    return ret;
+  }
+
+  operator symbolslist_t() const {
+    symbolslist_t ret;
+    ret.symbols = at("symbols");
+    return ret;
+  }
+  
+  operator gm_start_level_t() const {
+    gm_start_level_t ret;
+    ret.account = string(at("account"));
+    ret.instance = at("instanceId");
+    // TODO instructions
+    ret.seconds_per_day = at("secondsPerTradingDay");
+    ret.tickers = at("tickers");
+    ret.venues = at("venues");
+    return ret;
+  }
 };
+
+optional<unit> sf_client::api_heartbeat() {
+  TRACE;
+  optional<unit> ret;
+
+  auto res = m_client->request("GET", "/ob/api/heartbeat").get();
+  auto doc = res.extract_json().get();
+  TRACE;
+  return parser{doc};
+}
+
+optional<symbolslist_t> sf_client::venue_stocks(const std::string& venue) {
+  TRACE;
+  optional<symbolslist_t> ret;
+
+  auto res = m_client->request("GET", "/ob/api/venues/"+venue+"/stocks").get();
+  auto doc = res.extract_json().get();
+
+  TRACE;
+  return parser{doc};
+}
+
+void parse_into(const json::array& arr, std::vector<request_t>& reqs) {
+  for (auto&& req : arr) {
+    auto& reqobj = req.as_object();
+    reqs.push_back(request_t{
+	reqobj.at("price").as_number().to_uint64(),
+	  reqobj.at("qty").as_number().to_uint64()});
+  }
+}
+
+optional<orderbook_t> sf_client::orderbook_for_venue(const std::string& venue, const std::string& stock) {
+  TRACE;
+  optional<orderbook_t> ret;
+  
+  auto res = m_client->request("GET", "/ob/api/venues/"+venue+"/stocks/"+stock).get();
+  auto doc = res.extract_json().get();
+
+  auto& obj = doc.as_object();
+  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
+  switch (ret.ok) {
+  case ERROR:
+    ret.err = obj["error"].as_string();
+    break;
+  case OK: {
+    if (!obj["bids"].is_null()) parse_into(obj["bids"].as_array(), ret.data.bids);
+    if (!obj["asks"].is_null()) parse_into(obj["asks"].as_array(), ret.data.asks);
+    ret.data.timestamp = obj["ts"].as_string();
+    break;
+  }
+  }
+
+  TRACE;
+  return ret;
+}
+
+json::value order_to_json(const order_request_t& order) {
+  std::vector<std::pair<std::string, json::value>> fields;
+  fields.emplace_back("account", json::value::string(order.account));
+  fields.emplace_back("venue", json::value::string(order.venue));
+  fields.emplace_back("stock", json::value::string(order.symbol));
+  fields.emplace_back("price", json::value::number(order.req.price));
+  fields.emplace_back("qty", json::value::number(order.req.qty));
+  fields.emplace_back("direction", json::value::string(order.direction == BUY ? "buy" : "sell"));
+  fields.emplace_back("orderType", json::value::string(stringify(order.type)));
+  return json::value::object(move(fields));
+}
+
+template<class F>
+vector<result_of_t<F(const json::value&)>> map(const json::value& v, F f) {
+  using v_t = vector<result_of_t<F(const json::value&)>>;
+  v_t ret;
+
+  if (v.is_null())
+    return ret;
+
+  for (auto&& e : v.as_array())
+    ret.push_back(f(e));
+  
+  return ret;
+}
 
 optional<order_response_t> sf_client::post_order(const order_request_t& order) {
   TRACE;
@@ -252,14 +261,6 @@ optional<order_response_t> sf_client::cancel_order(const std::string& venue,
   return parser{doc};
 }
 
-vector<string> parse_stringarr(const json::value& v) {
-  vector<string> ret;
-  for (auto&& e : v.as_array()) {
-    ret.push_back(e.as_string());
-  }
-  return ret;
-}
-
 optional<gm_start_level_t> sf_client::start_level(const std::string& level) {
   TRACE;
   optional<gm_start_level_t> ret;
@@ -273,26 +274,8 @@ optional<gm_start_level_t> sf_client::start_level(const std::string& level) {
   auto res = m_client->request(req).get();
   auto doc = res.extract_json().get();
 
-  auto& obj = doc.as_object();
-  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
-
-  switch (ret.ok) {
-  case ERROR:
-    ret.err = obj["error"].as_string();
-    break;
-  case OK: {
-    ret->account = obj["account"].as_string();
-    ret->instance = obj["instanceId"].as_number().to_uint64();
-    // TODO instructions
-    ret->seconds_per_day = obj["secondsPerTradingDay"].as_number().to_uint64();
-    ret->tickers = parse_stringarr(obj["tickers"]);
-    ret->venues = parse_stringarr(obj["venues"]);
-    break;
-  }
-  }
-
   TRACE;
-  return ret;
+  return parser{doc};
 }
 
 optional<gm_start_level_t> sf_client::restart_level(uint64_t id) {
@@ -308,24 +291,6 @@ optional<gm_start_level_t> sf_client::restart_level(uint64_t id) {
   auto res = m_client->request(req).get();
   auto doc = res.extract_json().get();
 
-  auto& obj = doc.as_object();
-  ret.ok = obj["ok"].as_bool() ? OK : ERROR;
-
-  switch (ret.ok) {
-  case ERROR:
-    ret.err = obj["error"].as_string();
-    break;
-  case OK: {
-    ret->account = obj["account"].as_string();
-    ret->instance = obj["instanceId"].as_number().to_uint64();
-    // TODO instructions
-    ret->seconds_per_day = obj["secondsPerTradingDay"].as_number().to_uint64();
-    ret->tickers = parse_stringarr(obj["tickers"]);
-    ret->venues = parse_stringarr(obj["venues"]);
-    break;
-  }
-  }
-
   TRACE;
-  return ret;
+  return parser{doc};
 }
